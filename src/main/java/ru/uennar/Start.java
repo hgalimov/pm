151976d9bc54db56e7088b04b93094855c293b2a
Khalim Galimov <khalimgal@gmail.com>
2020-04-01 19:28:04 +0300

    test scope prototype

--- src/main/java/ru/uennar/Start.java
+++ src/main/java/ru/uennar/Start.java
@@ -8,10 +8,15 @@ public class Start {
                 "applicationContext.xml");
         //MeasEqui equi = context.getBean("equiBean", MeasEqui.class);
         //EquiMaster equiMaster = new EquiMaster(equi);
-        EquiMaster equiMaster = context.getBean("equiMaster", EquiMaster.class);
-        equiMaster.getEquiBrand();
-        System.out.println("Пользователь, который обрабатывает оборудования: " +
-                equiMaster.getUserName() + ". Смена: " + equiMaster.getShift());
+        EquiMaster equiMaster1 = context.getBean("equiMaster", EquiMaster.class);
+        EquiMaster equiMaster2 = context.getBean("equiMaster", EquiMaster.class);
+        System.out.println(equiMaster1 == equiMaster2);
+        equiMaster1.getEquiBrand();
+        equiMaster2.setUserName("superAdmin");
+        equiMaster2.setShift(2);
+        System.out.println("Пользователи, которые обрабатывают оборудования: " + '\n' +
+                equiMaster1.getUserName() + ". Смена: " + equiMaster1.getShift() + '\n' +
+                equiMaster2.getUserName() + ". Смена: " + equiMaster2.getShift());
         context.close();
     }
 }

c2f5057a020ec0d0d102e6aa6cc978751c84504d
Khalim Galimov <khalimgal@gmail.com>
2020-04-03 16:43:51 +0300

    using annotation Component

--- src/main/java/ru/uennar/ElectricEqui.java
+++ src/main/java/ru/uennar/ElectricEqui.java
@@ -1,5 +1,8 @@
 package ru.uennar;
 
+import org.springframework.stereotype.Component;
+
+
 public class ElectricEqui implements Equi{
     private ElectricEqui() {
     }
--- src/main/java/ru/uennar/MeasEqui.java
+++ src/main/java/ru/uennar/MeasEqui.java
@@ -1,5 +1,8 @@
 package ru.uennar;
 
+import org.springframework.stereotype.Component;
+
+@Component("measEqui")
 public class MeasEqui implements Equi{
     @Override
     public String getBrand() {

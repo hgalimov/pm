2541e5ab9ce977e8265e5e00d20b70432037680c
Khalim Galimov <khalimgal@gmail.com>
2020-04-09 16:24:31 +0300

    using annotations in controller

--- src/main/java/ru/uennar/controllers/StartController.java
+++ src/main/java/ru/uennar/controllers/StartController.java
@@ -1,17 +1,25 @@
-package ru.uennar;
+package ru.uennar.controllers;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.uennar.SpringConfig;
+import ru.uennar.User;
 
 @Controller
+@RequestMapping("/start")
 public class StartController {
     @GetMapping("/getInfo")
     public String getInfo(ModelMap model){
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
         User user = context.getBean("user", User.class);
         model.addAttribute("msg", user.toString());
-        return "getInfo";
+        return "/start/getInfo";
+    }
+    @GetMapping("/start")
+    public String start(){
+        return "/start/start";
     }
 }
